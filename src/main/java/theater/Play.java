c4e5c3f061203255e29e7db8b877f5865e436384
Collin <230224879+collingau@users.noreply.github.com>
2025-11-21 14:27:37 -0500

    Task 1.1 fixed CheckStyle issues

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,24 @@
 package theater;
 
+/**
+ * Represents a play in the theater system.
+ * Each play has a name and a type (e.g., tragedy or comedy).
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
